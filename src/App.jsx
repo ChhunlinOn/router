3af40ce06daca62c9b72b733df2d0afd43ad1836
ChhunlinOn <chhunlin.on@institute.pse.ngo>
2024-07-31 10:16:44 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
